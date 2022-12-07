0d107b514572f90ef594673774c5a94012ace273
Johan Bernhardsson <johan@johanbernhardsson.se>
2022-12-07 07:24:38 +0100

    Day 07 second gold star :)

--- Day07/Day07.cpp
+++ Day07/Day07.cpp
@@ -7,8 +7,8 @@
 #include <string>
 #include <vector>
 
-//std::string Filename = "TestInput.txt";
-std::string Filename = "Input.txt";
+std::string Filename = "TestInput.txt";
+//std::string Filename = "Input.txt";
 
 struct Directory
 {
@@ -31,32 +31,32 @@ struct Directory
         return size;
     }
     
-    std::vector<std::pair<Directory*, int64_t>> GetDirectoriesWithMaxSize(int64_t InMaxSize)
+    std::vector<std::pair<Directory*, int64_t>> GetDirectoriesWithMinSize(int64_t InMinSize)
     {
         std::vector<std::pair<Directory*, int64_t>> returnVector;
         const int64_t directorySize = GetDirectorySize();
-        if (directorySize < InMaxSize)
+        if (directorySize >= InMinSize)
             returnVector.emplace_back(this, directorySize);
         
         for(const auto& dir : Directories)
         {
-            auto foundSubDirectories = dir->GetDirectoriesWithMaxSize(InMaxSize);
+            auto foundSubDirectories = dir->GetDirectoriesWithMinSize(InMinSize);
             returnVector.insert(returnVector.end(), foundSubDirectories.begin(), foundSubDirectories.end());
         }
         return returnVector;
     }
 };
 
-std::shared_ptr<Directory> ParseDirectory(std::ifstream& inputFile, std::string InName = "")
+std::shared_ptr<Directory> ParseDirectory(std::ifstream& InInputFile, std::string InName = "")
 {
     std::shared_ptr<Directory> returnPointer;
     if (!InName.empty())
         returnPointer = std::make_shared<Directory>(InName);
     
     std::string inputLine;
-    while (!inputFile.eof())
+    while (!InInputFile.eof())
     {
-        std::getline(inputFile, inputLine);
+        std::getline(InInputFile, inputLine);
         std::vector<std::string> parts;
         std::istringstream stringParser(inputLine);
         std::string lineBuffer;
@@ -71,38 +71,37 @@ std::shared_ptr<Directory> ParseDirectory(std::ifstream& inputFile, std::string 
                     return returnPointer;
                 
                 if (returnPointer == nullptr)
-                    returnPointer = ParseDirectory(inputFile, parts[2]);
+                    returnPointer = ParseDirectory(InInputFile, parts[2]);
                 else
-                    returnPointer->Directories.emplace_back(ParseDirectory(inputFile, parts[2]));
+                    returnPointer->Directories.emplace_back(ParseDirectory(InInputFile, parts[2]));
             }
         }
-        else
+        else if (parts[0] != "dir")
         {
-            if (parts[0] != "dir")
-            {
-                returnPointer->ContainingFiles.emplace_back(parts[1], std::atoll(parts[0].c_str()));
-            }
+            returnPointer->ContainingFiles.emplace_back(parts[1], std::atoll(parts[0].c_str()));
         }
     }
     return returnPointer;
 }
 
-int main(int argc, char* argv[])
+int main(int /*InArgc*/, char* /*InArgv[]*/)
 {
     std::ifstream inputFile;
 
     inputFile.open(Filename);
     std::shared_ptr<Directory> startDir;
-    auto tree = ParseDirectory(inputFile);
-    auto t = tree->GetDirectoriesWithMaxSize(100000);
+    const int64_t diskSize = 70000000;
+    
+    const auto tree = ParseDirectory(inputFile);
+    const auto amountToFree = tree->GetDirectorySize() + 30000000 - diskSize;
 
-    int64_t size = 0;
-    for (auto& dir : t)
+    auto foundDirectories = tree->GetDirectoriesWithMinSize(amountToFree);
+    std::ranges::sort(foundDirectories, [](const auto& InA, const auto& InB)
     {
-        size += dir.second;
-    }
+        return InA.second < InB.second;
+    });
 
-    std::cout << "Total size: " << size << std::endl;
+    std::cout << "Remove dir: " << foundDirectories[0].first->Name << " of size " << foundDirectories[0].second << std::endl;
     
     return 0;
 }
