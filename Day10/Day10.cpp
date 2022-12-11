64fc2b6708de6087b7cbaddbaa9e06f1e28d45ea
Johan Bernhardsson <johan@johanbernhardsson.se>
2022-12-11 11:41:49 +0100

    Day 10 second gold star :)

--- Day10/Day10.cpp
+++ Day10/Day10.cpp
@@ -1,10 +1,11 @@
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-//std::string Filename = "TestInput.txt";
-std::string Filename = "Input.txt";
+std::string Filename = "TestInput.txt";
+//std::string Filename = "Input.txt";
 
 int main(int /*InArgc*/, char* /*InArgv[]*/)
 {
@@ -14,7 +15,7 @@ int main(int /*InArgc*/, char* /*InArgv[]*/)
     std::string inputLine;
     int cycle = 0;
     int32_t registerX = 1;
-    int32_t sumSignalStrength = 0;
+    std::array<char, 240> crtDraw{'E'};
     while (!inputFile.eof())
     {
         std::getline(inputFile, inputLine);
@@ -25,19 +26,19 @@ int main(int /*InArgc*/, char* /*InArgv[]*/)
         int instructionCount = instruction[0] == 'a' ? 2 : 1;
         for (int a = 0; a <instructionCount; ++a)
         {
+            crtDraw[cycle] = std::abs(registerX - cycle % 40) <= 1 ?  '#' : '.';
             cycle++;
-
-            if (((cycle - 20) % 40) == 0)
-            {
-                std::cout << "Check " << cycle << " Value: " << registerX << std::endl;
-                sumSignalStrength += cycle * registerX;
-            }
             if (a == 1)
                 registerX += value;
         }
     }
-    
-    std::cout << "Sum signal strength: " << sumSignalStrength << std::endl;
+
+    for (int y = 0; y < 6; ++y)
+    {
+        for (int x = 0; x < 40; ++x)
+            std::cout << crtDraw[x + y * 40];
+        std::cout << std::endl;
+    }
     
     return 0;
 }
