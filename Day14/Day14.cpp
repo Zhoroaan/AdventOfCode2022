8e59f7f947b6142df18464c76d57735f2f6d51a6
Johan Bernhardsson <johan@johanbernhardsson.se>
2022-12-27 17:29:06 +0100

    Day 14 first gold star :)

---
+++ Day14/Day14.cpp
@@ -0,0 +1,133 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <ranges>
+#include <sstream>
+#include <vector>
+
+//std::string Filename = "TestInput.txt";
+std::string Filename = "Input.txt";
+constexpr int32_t GridSize = 1200;
+
+class Grid
+{
+public:
+    Grid () : GridData(GridSize * GridSize)
+    {
+        std::memset(GridData.data(), '.', GridSize * GridSize);
+    }
+    const char* operator[](int x) const
+    {
+        return &GridData[x * GridSize];
+    }
+    char* operator[](int x)
+    {
+        return &GridData[x * GridSize];
+    }
+
+    void Print()
+    {
+        for (int y = 0; y < 10; ++y)
+        {
+            for (int x = 494; x < 504; ++x)
+            {
+                std::cout << (*this)[x][y];
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    bool RunSand()
+    {
+        Grid& grid = *this;
+        int x = 500, y = 0;
+        while (true)
+        {
+            if (y > GridSize)
+                return false;
+            
+            if (grid[x][y+1] == '.')
+            {
+                y++;
+            }
+            else if (grid[x - 1][y + 1] == '.')
+            {
+                x--;
+                y++;
+            }
+            else if (grid[x + 1][y + 1] == '.')
+            {
+                x++;
+                y++;
+            }
+            else
+            {
+                grid[x][y] = 'o';
+                break;
+            }
+        }
+        return true;
+    }
+
+protected:
+    std::vector<char> GridData;
+};
+
+void DrawLine(Grid& InGrid, int InPrevX, int InPrevY, int InX, int InY)
+{
+    std::cout << "Draw line from " << InPrevX << "," << InPrevY << " to " << InX << "," << InY << std::endl;
+    if (InPrevX != InX)
+    {
+        for (int x = std::min(InPrevX, InX); x <=std::max(InPrevX, InX); ++x)
+            InGrid[x][InY] = '#';
+    }
+    else
+    {
+        for (int y = std::min(InPrevY, InY); y <=std::max(InPrevY, InY); ++y)
+            InGrid[InX][y] = '#';
+    }
+}
+
+int main(int /*InArgc*/, char* /*InArgv[]*/)
+{
+    std::ifstream inputFile;
+
+    Grid grid;
+    grid[0][0] = 'p';
+    grid[0][1] = 'o';
+    
+    inputFile.open(Filename);
+    std::string inputLine;
+    while (inputFile.is_open() && !inputFile.eof())
+    {
+        std::getline(inputFile, inputLine);
+        std::string_view words{inputLine};
+        constexpr std::string_view blockDelim{" -> "};
+
+        int prevX = -1, prevY = -1;
+        
+        for (const auto word : std::views::split(words, blockDelim))
+        {
+            std::string entry(word.begin(), word.end());
+            std::stringstream ss(entry);
+            int x = 0, y = 0;
+            ss >> x, ss.ignore(1), ss >> y;
+            bool newValue = prevY == -1;
+            if (!newValue)
+            {
+                // Draw Line
+                DrawLine(grid, prevX, prevY, x, y);
+            }
+            prevX = x;
+            prevY = y;
+        }
+    }
+    grid[500][0] = '+';
+    grid.Print();
+    int32_t counter = 0;
+    while (grid.RunSand())
+        counter++;
+    
+    std::cout << "Num stacked: " << counter << std::endl;
+    return 0;
+}
